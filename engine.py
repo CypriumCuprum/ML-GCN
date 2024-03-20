20c9e9bcd5e369000f01e8ad5be85eebbe7e70e7
Vu Ket Dong <106828064+CypriumCuprum@users.noreply.github.com>
2024-03-20 10:27:34 +0700

    Update engine.py

--- engine.py
+++ engine.py
@@ -313,7 +313,7 @@ class Engine(object):
     def adjust_learning_rate(self, optimizer):
         """Sets the learning rate to the initial LR decayed by 10 every 30 epochs"""
         lr_list = []
-        decay = 0.1 if sum(self.state['epoch'] == np.array(self.state['epoch_step'])) > 0 else 1.0
+        decay = 0.1 if self.state['epoch'] == np.array(self.state['epoch_step']) else 1.0
         for param_group in optimizer.param_groups:
             param_group['lr'] = param_group['lr'] * decay
             lr_list.append(param_group['lr'])
